15439c578af231586fdbb533f1daf23b57ee9915
supertestnet <58400631+supertestnet@users.noreply.github.com>
2024-11-01 16:14:57 -0400

    Add files via upload

---
+++ new_scripts.js
@@ -0,0 +1,80 @@
+$( '.title_and_subtitle' ).onclick = () => {window.location.href = './';}
+$( '.lang_btn' ).onclick = () => {window.location.href = window.location.protocol + "//" + window.location.hostname + window.location.pathname + "?lang=english";}
+$( '.contacto_btn' ).onclick = () => {window.location.href = "./contacto.html";}
+if ( window.location.href.includes( "lang=english" ) ) {
+    $( '.title_and_subtitle' ).onclick = () => {window.location.href = './' + "?lang=english";}
+    $( '.subtitle' ).innerText = `Friday November 29 - Sunday December 1, 2024`;
+    $( '.lang_btn span' ).innerText = `Español`;
+    $( '.lang_btn' ).onclick = () => {window.location.href = window.location.protocol + "//" + window.location.hostname + window.location.pathname;}
+    $( '.contacto_btn span' ).innerText = `Contact`;
+    $( '.contacto_btn' ).onclick = () => {window.location.href = "./contacto.html?lang=english";}
+
+    // translation logic
+    var idx = window.location.pathname.lastIndexOf( "/" );
+    var page_were_on = window.location.pathname.substring( idx + 1 );
+    if ( page_were_on === "" || page_were_on === "new_hackalajara_webpage.html" || page_were_on === "new_index.html" || page_were_on === "index.html" ) {
+        $( '.que_es_label' ).innerText = `What is it?`;
+        $( '.que_es_descripcion' ).innerText = `Come to the first bitcoin-focused hackathon in Guadalajara, Mexico! Have an idea for how to improve bitcoin? Want to meet other bitcoiners and hack something out? Join us and help make the dream of sound, digital money come true!`;
+        $( '.donde_es_label' ).innerText = `Where is it?`;
+        $( '.categorias_label' ).innerText = `Prize categories`;
+        $( '.categorias' ).innerHTML = `
+            <li>Best programming hack - prize of 0.013 BTC (valued at $18,500 MXN currently)</li>
+            <li>Best design hack - prize of 0.013 BTC (valued at $18,500 MXN currently)</li>
+            <li>Best education hack - prize of 0.013 BTC (valued at $18,500 MXN currently)</li>
+        `;
+        $( '.registrar_btn span' ).innerText = `Register here`;
+        $( '.mentores_label' ).innerText = `Mentors`;
+        $( '.mentores_descripcion' ).innerText = `For those who aren't yet comfortable coding or aren't sure what to do, we'll have mentors available to help you! They will walk around the area while folks are coding and you can flag them down and say, "What's a good project idea?" or "Can you help me find a team?" or "I'm not sure how to solve this programming problem" and they will help you out. Confirmed mentors so far include:`;
+        $( '.mentores_pgraph' ).innerHTML = `
+            <p class="uppercase pgraph">CAR GONZALEZ - DESIGNER IN BITCOIN AND LEADER OF PLEBLAB IN AUSTIN, TEXAS</p>
+            <p class="uppercase pgraph">TOPHER SCOTT - CREATOR OF CRYPTOGRAPHIC LIBRARIES AND CTO OF BITESCROW</p>
+            <p class="uppercase pgraph">SUPER TESTNET - EXPERT IN BITCOIN SCRIPT AND DEVELOPER OF LAYER TWO PROTOCOLS</p>
+        `;
+        $( '.horario_label' ).innerText = `SCHEDULE`;
+        $( '.horario_parte_1' ).innerHTML = `
+            <span style="font-family: Ultrabold_GraublauWeb;">THURSDAY</span><br>
+            2:00 PM - LARP IN THE PARK<br><br>
+
+            <span style="font-family: Ultrabold_GraublauWeb;">FRIDAY</span><br>
+            2:00 PM - POTENTIAL WORKSHOP<br>
+            3:00 PM - KAPITALEX WORKSHOP<br>
+            4:00 PM - BREAK<br>
+            5:00 PM - OPENING CEREMONY<br>
+            5:30 PM - TEAM FORMATION, IDEATION, AND BEGIN HACK<br>
+            7:00 PM - END OF HACK FOR THE DAY<br><br>
+
+            <span style="font-family: Ultrabold_GraublauWeb;">SATURDAY</span><br>
+            9:00 AM - START OF HACK FOR THE DAY<br>
+            9:00 AM - (VERY OPTIONAL) PITCH WORKSHOP<br>
+            7:00 PM - END OF HACK FOR THE DAY<br><br>
+
+            <span style="font-family: Ultrabold_GraublauWeb;">SUNDAY</span><br>
+            9:00 AM - BEGIN HACK FOR THE DAY<br>
+            10:30 AM - END HACK FOR THE DAY AND SUBMIT PROJECTS/SLIDESHOWS<br>
+            11:00 AM - PRESENTATIONS<br>
+            TO BE ANNOUNCED - JUDGING PERIOD<br>
+            TO BE ANNOUNCED - PRESENT WINNERS<br>
+        `;
+        $( '.alojamientos_label' ).innerText = `ACCOMODATIONS`;
+    }
+    if ( page_were_on === "contacto.html" ) {
+        $( '.donaciones_label' ).innerText = `Donations`;
+        $( '.apoyos' ).innerHTML = `
+        <p>Sponsorship supports</p>
+        <ul>
+            <li>Prize Money Hackthon (3 categories)</li>
+            <li>Local Bitcoin and LN Developers</li>
+            <li>Venue and Facilities</li>
+            <li>Travel Grants</li>
+            <li>Stipdends for Devs from other communities in Mexico</li>
+        </ul>
+        `;
+        $( '.contacto_label' ).innerText = `Contact`;
+        $( '.contacto_ul' ).innerHTML = `
+        <li><a href="https://twitter.com/super_testnet" target="_blank">@super_testnet</a> on twitter</li>
+        <li><a href="https://t.me/supertestnet" target="_blank">@supertestnet</a> on telegram</li>
+        <li><a href="https://njump.me/nprofile1qy88wumn8ghj7mn0wvhxcmmv9uq32amnwvaz7tmwdaehgu3wdau8gu3wv3jhvtcpz4mhxue69uhhyetvv9ujumn0wd68ytnzvuhsqgpps055wkzgr583ynaaj0zkej4ytel9gh8whr2jsj8esfflf9aew5p96hus" target="_blank">Super Testnet</a> on nostr</li>
+        <li><a href="https://www.youtube.com/@highlevelbitcoin" target="_blank">High Level Bitcoin</a> on youtube</li>
+        `;
+    }
+}
